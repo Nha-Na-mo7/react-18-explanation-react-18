7a05580898b0b8dfa27c4aa546fba90b523ded0d
Nha-Na-mo7 <zirconiump42@gmail.com>
2023-10-21 20:12:22 +0900

    feat: add console

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    console.log("useEffect App が レンダリングされました。(@v18)");
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
