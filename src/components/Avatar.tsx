93bb9b3cdb6d34b8a5812e5bddecce56d8d41dc8
Nha-Na-mo7 <zirconiump42@gmail.com>
2023-10-25 23:09:28 +0900

    feat: create Avatar.tsx

---
+++ src/components/Avatar.tsx
@@ -0,0 +1,17 @@
+export const Avatar = () => {
+  return (
+    <div
+      style={{
+        width: "30px",
+        height: "30px",
+        border: "1px solid gray",
+        borderRadius: "50%",
+        textAlign: "center",
+        lineHeight: "1.8",
+        userSelect: "none",
+      }}
+    >
+      {"A"}
+    </div>
+  );
+};
