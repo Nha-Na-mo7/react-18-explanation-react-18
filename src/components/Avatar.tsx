1245daff5d7d1925776867631e40baab1cbe3feb
Nha-Na-mo7 <zirconiump42@gmail.com>
2023-10-26 19:11:48 +0900

    feat: Transition と Avatarを追加

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,17 +1,28 @@
-export const Avatar = () => {
+import { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+  isSelected?: boolean;
+  onClick: (assignee: string) => void;
+};
+
+export const Avatar = ({ children, isSelected = false, onClick }: Props) => {
+  const border = isSelected ? "3px solid orange" : "1px solid gray";
+
   return (
     <div
       style={{
         width: "30px",
         height: "30px",
-        border: "1px solid gray",
+        border,
         borderRadius: "50%",
         textAlign: "center",
         lineHeight: "1.8",
         userSelect: "none",
       }}
+      onClick={() => onClick(`${children}`)}
     >
-      {"A"}
+      {children}
     </div>
   );
 };
